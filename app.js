7318644088c3655b69100b8720dfddf17646d57f
Erick Vicentin <erickvicentin.globant@latam.com>
2021-06-30 01:42:09 -0300

    Added travis.yml

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ app.get("/", function (req, res) {
   res.send("Hello World! Hola Mundo!");
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3001, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log("App escuchando a http://%s:%s", host, port);
+  console.log("App corriendo en puerto ", host, port);
 });
