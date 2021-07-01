ca7a7e56e031e6cf722fefb7149d420626020e15
Erick Vicentin <erickvicentin.globant@latam.com>
2021-06-30 21:51:56 -0300

    Added random parameters

--- index.test.js
+++ index.test.js
@@ -2,16 +2,24 @@ const {sum, subtraction} = require("./index")
 const assert = require("assert").strict;
 
 describe("Sum Test", function () {
-    it("return the sum from values a & b", () => {
-        let res = sum(1, 2);
-        assert.deepEqual(res, 3);
+    it("return the sum from random values a & b", () => {
+        const a = Math.floor(Math.random() * 10)
+        const b = Math.floor(Math.random() * 10)
+        const expected = a+b
+        let result = sum(a, b);
+        console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
+        assert.deepEqual(result, expected);
     });
 });
 
 describe("Substraction Test", function () {
-    it("return the subtraction from values a & b", () => {
-        let res = subtraction(2, 2);
-        assert.deepEqual(res, 0);
+    it("return the subtraction from random values a & b", () => {
+        const a = Math.floor(Math.random() * 10)
+        const b = Math.floor(Math.random() * 10)
+        const expected = a-b
+        let result = subtraction(a, b);
+        console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
+        assert.deepEqual(result, expected);
     });
 });
 
