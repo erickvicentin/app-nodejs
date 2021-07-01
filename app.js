7195ed5f8bf306daf420a54bc71f127bc26521a3
Erick Vicentin <erickvicentin.globant@latam.com>
2021-07-01 10:35:42 -0300

    Updated package.json

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = process.env.port || 3003;
+const port = process.env.port || 5000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World! We are deployed!!');
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
