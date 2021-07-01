cc68f3466679c6d7595ade75e1f1f1064ec56636
Erick Vicentin <erickvicentin.globant@latam.com>
2021-07-01 12:58:07 -0300

    Updated index.js

--- index.test.js
+++ index.test.js
@@ -1,24 +1,21 @@
 const {sum, subtraction} = require("./index")
 const assert = require("assert").strict;
-const crypto = require('crypto')
-
-const buf = crypto.pseudoRandomBytes(2); // Compliant for security-sensitive use cases
-
-let a = parseInt(buf[0]); let b = parseInt(buf[1])
 
 describe("Sum Test", function () {
     it("return the sum from random values a & b", () => {
+        const a = Math.floor(Math.random() * 10)
+        const b = Math.floor(Math.random() * 10)
         const expected = a+b
         let result = sum(a, b);
-        console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
-        console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
         console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
         assert.deepEqual(result, expected);
     });
 });
 
-describe("Substraction Test", function () {
+deribe("Substraction Test", function () {
     it("return the subtraction from random values a & b", () => {
+        const a = Math.floor(Math.random() * 10)
+        const b = Math.floor(Math.random() * 10)
         const expected = a-b
         let result = subtraction(a, b);
         console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
