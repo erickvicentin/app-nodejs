523681c066ff5f8618e22dcebb0cdddca5aff75c
Erick Vicentin <erickvicentin.globant@latam.com>
2021-07-01 01:37:17 -0300

    Improvement node app

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
 
+const port = process.env.port || 3001
+const hostname = process.env.HOST || "127.0.0.1";
+
+
 app.get("/", function (req, res) {
-  res.send("Hello World! Hola Mundo!");
+  res.statusCode = 200;
+  res.set('Content-Type', 'text/html')
+  res.send(Buffer.from('<h1>Hello World! Hola Mundo!</h1>' + '<h2>We are deployed!</h2>'))
 });
 
-var server = app.listen(process.env.PORT || 3001, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("App corriendo en puerto ", host, port);
+var server = app.listen(port, function () {
+   console.log("We are live on port ", hostname, port);
 });
