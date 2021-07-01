acc80d96fed0e9a1584455078631422555e67975
Erick Vicentin <erickvicentin.globant@latam.com>
2021-07-01 12:08:05 -0300

    Improve with sonarcloud recommendation

--- index.test.js
+++ index.test.js
@@ -1,10 +1,13 @@
 const {sum, subtraction} = require("./index")
 const assert = require("assert").strict;
+const crypto = require('crypto')
+
+const buf = crypto.pseudoRandomBytes(2); // Compliant for security-sensitive use cases
+
+let a = parseInt(buf[0]); let b = parseInt(buf[1])
 
 describe("Sum Test", function () {
     it("return the sum from random values a & b", () => {
-        const a = Math.floor(Math.random() * 10)
-        const b = Math.floor(Math.random() * 10)
         const expected = a+b
         let result = sum(a, b);
         console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
@@ -14,8 +17,6 @@ describe("Sum Test", function () {
 
 describe("Substraction Test", function () {
     it("return the subtraction from random values a & b", () => {
-        const a = Math.floor(Math.random() * 10)
-        const b = Math.floor(Math.random() * 10)
         const expected = a-b
         let result = subtraction(a, b);
         console.log(`Value A: ${a}, Value B: ${b}, Expected: ${expected}, Result: ${result}`)
